3108064045f18273e9e5bec25ea200cb337e0e98
amohamed-alt <a.mohamed@bayt.net>
2026-07-27 21:31:16 +0300

    Add builder suite page

---
+++ apps/web/app/builder/page.js
@@ -0,0 +1,7 @@
+import { BuilderSuiteClient } from '@/components/sdr/BuilderSuiteClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function BuilderPage() {
+  return <BuilderSuiteClient />;
+}
