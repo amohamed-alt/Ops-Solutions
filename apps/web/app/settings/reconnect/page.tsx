20694a717513226cc25596ebf85d81202ddee20c
amohamed-alt <a.mohamed@bayt.net>
2026-07-28 11:15:56 +0300

    Add HubSpot access settings page

---
+++ apps/web/app/settings/reconnect/page.tsx
@@ -0,0 +1,7 @@
+import { HubSpotReconnectClient } from '../../../components/sdr/HubSpotReconnectClient';
+
+export const dynamic = 'force-dynamic';
+
+export default function HubSpotReconnectPage() {
+  return <HubSpotReconnectClient />;
+}
